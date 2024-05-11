fc1829460debc65ecfade55d165b387fe293d6d8
QAEZZ <russian.devv@gmail.com>
2024-05-11 12:06:24 -0500

    healthbar

--- src/entities/player.py
+++ src/entities/player.py
@@ -1,6 +1,59 @@
-from ursina import Entity, camera, lerp, time, held_keys, mouse, Vec3, clamp, color
+from ursina import Entity, camera, lerp, time, Text, held_keys, mouse, curve, Vec3, clamp, color, Button, window, Default, Quad, Grid
 from ursina.prefabs.first_person_controller import FirstPersonController
-from ursina.prefabs.health_bar import HealthBar
+from ursina.scripts.property_generator import generate_properties_for_class
+
+@generate_properties_for_class()
+class HealthBar(Button):
+
+    def __init__(self, max_value=100, value=Default, roundness=.25, animation_duration=.1, show_text=True, show_lines=False, text_size=.7, origin=(-.5,.5), **kwargs):
+        super().__init__(
+            position=(-.49*window.aspect_ratio,.48),
+            scale=(Text.size*7.25,Text.size),
+            origin=origin,
+            color=color.black,
+            text='Health: hp / max hp',
+            text_size=text_size,
+            radius=roundness,
+            ignore=True
+        )
+
+        # self.bar = Entity(parent=self, model=Quad(radius=roundness), origin=origin, z=-.005, color=color.red.tint(-.2), ignore=True)
+        # self.lines = Entity(parent=self.bar, y=-1, color=color.black33, ignore=True, enabled=show_lines, z=-.05)
+
+        self.max_value = max_value
+        self.clamp = False
+        self.roundness = roundness
+        self.animation_duration = animation_duration
+        self.show_lines = show_lines
+        self.show_text = show_text
+        self.value = self.max_value if value == Default else value
+
+        for key, value in kwargs.items():
+            setattr(self, key, value)
+
+        self.text_entity.enabled = show_text
+
+
+    def value_setter(self, n):
+        if self.clamp:
+            n = clamp(n, 0, self.max_value)
+
+        self._value = n
+        self.text_entity.text = f'Health: {n} / {self.max_value}'
+
+    def show_text_getter(self):
+        return self.text_entity.enabled
+    def show_text_setter(self, value):
+        self.text_entity.enabled = value
+
+    def __setattr__(self, name, value):
+        if 'scale' and hasattr(self, 'model') and self.model:
+            self.model.aspect = self.world_scale_x / self.world_scale_y
+            self.model.generate()
+            if hasattr(self, 'text_entity') and self.text_entity:
+                self.text_entity.world_scale = 25 * self.text_size
+
+        super().__setattr__(name, value)
 
 class Player(Entity):
     def __init__(self, **kwargs):
@@ -22,10 +75,10 @@ class Player(Entity):
         self.is_sliding = False
         self.target_rotation = self.controller.rotation_y
         self.target_camera_pivot_y = self.defaultCameraPivotY
-        self.turning_speed = 50
+        self.turning_speed = 75
         
         self.health = 100
-        self.__healthbar = HealthBar(max_value=100, value=self.health)
+        self.__healthbar = HealthBar(max_value=100, value=self.health, roundess=.25)
 
         self.controller.speed = self.defaultSpeed
         self.controller.mouse_sensitivity = self.defaultSensitivity
--- src/main.py
+++ src/main.py
@@ -2,7 +2,8 @@ from entities.player import Player
 from direct.stdpy import thread
 from ursina import (AmbientLight, Button, EditorCamera, Entity, PointLight,
                     Sky, SmoothFollow, Ursina, Vec3, Vec2, application, camera,
-                    color, destroy, mouse, random, scene, Text)
+                    color, destroy, mouse, random, scene, Text, DirectionalLight)
+from ursina.shaders import *
 
 
 def loadLevel():
@@ -20,22 +21,22 @@ def loadLevel():
 
     size = 150
     height = 25
-    ground = Entity(model='plane', scale=(size, 1, size), texture='floor', texture_scale=Vec2(24,24), collider='box', color=color.gray)
+    ground = Entity(model='plane', scale=(size, 1, size), texture='floor', texture_scale=Vec2(24,24), collider='box', color=color.gray, )
 
-    wall_1 = Entity(model='cube', scale=(size, height, 1), position=(0, 1, -size/2), texture='brick', texture_scale=Vec2(22,7), collider='mesh', color=color.gray)
-    wall_2 = Entity(model='cube', scale=(size, height, 1), position=(0, 1, size/2), texture='brick', texture_scale=Vec2(22,7), collider='mesh', color=color.gray)
-    wall_3 = Entity(model='cube', scale=(1, height, size), position=(-size/2, 1, 0), texture='brick', texture_scale=Vec2(22,7), collider='mesh', color=color.gray)
-    wall_4 = Entity(model='cube', scale=(1, height, size), position=(size/2, 1, 0), texture='brick', texture_scale=Vec2(22,7), collider='mesh', color=color.gray)
+    wall_1 = Entity(model='cube', scale=(size, height, 1), position=(0, 1, -size/2), texture='brick', texture_scale=Vec2(22,7), collider='mesh', color=color.gray, )
+    wall_2 = Entity(model='cube', scale=(size, height, 1), position=(0, 1, size/2), texture='brick', texture_scale=Vec2(22,7), collider='mesh', color=color.gray, )
+    wall_3 = Entity(model='cube', scale=(1, height, size), position=(-size/2, 1, 0), texture='brick', texture_scale=Vec2(22,7), collider='mesh', color=color.gray, )
+    wall_4 = Entity(model='cube', scale=(1, height, size), position=(size/2, 1, 0), texture='brick', texture_scale=Vec2(22,7), collider='mesh', color=color.gray, )
     
-    block = Entity(model='cube', scale=(12, 4, 12), position=((size/2)-6, 1.75, (size/2)-6), texture='light_floor', collider='mesh', color=color.gray)
-    ramp = Entity(model='cube', rotation_z = -10, scale=(32, 2, 12), position=((size/2)-27.5, 0, (size/2)-6), texture='light_floor', texture_scale=Vec2(2,1), collider='mesh', color=color.gray)
-    ramp_2 = Entity(model='cube', rotation_x = -10, scale=(12, 2, 32), position=((size/2)-6, 0, (size/2)-27.5), texture='light_floor', texture_scale=Vec2(1,2), collider='mesh', color=color.gray)
+    block = Entity(model='cube', scale=(12, 4, 12), position=((size/2)-6, 1.75, (size/2)-6), texture='light_floor', collider='mesh', color=color.gray, )
+    ramp = Entity(model='cube', rotation_z = -10, scale=(32, 2, 12), position=((size/2)-27.5, 0, (size/2)-6), texture='light_floor', texture_scale=Vec2(2,1), collider='mesh', color=color.gray, )
+    ramp_2 = Entity(model='cube', rotation_x = -10, scale=(12, 2, 32), position=((size/2)-6, 0, (size/2)-27.5), texture='light_floor', texture_scale=Vec2(1,2), collider='mesh', color=color.gray, )
 
-    spawn_pad = Entity(model='cube', scale=(10,10,10), position=(0, -2.5, 0), texture='light_floor', texture_scale=Vec2(3,3), collider='mesh', color=color.gray)
-    spawn_pad_ramp   = Entity(model='cube', rotation_z = -10, scale=(15,10,10), position=(-11.5, -3.75, 0), texture='light_floor', texture_scale=Vec2(4,3), collider='mesh', color=color.gray)
-    spawn_pad_ramp_1 = Entity(model='cube', rotation_z = 10, scale=(15,10,10), position=(11.5, -3.75, 0), texture='light_floor', texture_scale=Vec2(4,3), collider='mesh', color=color.gray)
-    spawn_pad_ramp_2 = Entity(model='cube', rotation_x = -10, scale=(10,10,15), position=(0, -3.75, -11.5), texture='light_floor', texture_scale=Vec2(3,4), collider='mesh', color=color.gray)
-    spawn_pad_ramp_3 = Entity(model='cube', rotation_x = 10, scale=(10,10,15), position=(0, -3.75, 11.5), texture='light_floor', texture_scale=Vec2(3,4), collider='mesh', color=color.gray)
+    spawn_pad = Entity(model='cube', scale=(10,10,10), position=(0, -2.5, 0), texture='light_floor', texture_scale=Vec2(3,3), collider='mesh', color=color.gray, )
+    spawn_pad_ramp   = Entity(model='cube', rotation_z = -10, scale=(15,10,10), position=(-11.5, -3.75, 0), texture='light_floor', texture_scale=Vec2(4,3), collider='mesh', color=color.gray, )
+    spawn_pad_ramp_1 = Entity(model='cube', rotation_z = 10, scale=(15,10,10), position=(11.5, -3.75, 0), texture='light_floor', texture_scale=Vec2(4,3), collider='mesh', color=color.gray, )
+    spawn_pad_ramp_2 = Entity(model='cube', rotation_x = -10, scale=(10,10,15), position=(0, -3.75, -11.5), texture='light_floor', texture_scale=Vec2(3,4), collider='mesh', color=color.gray, )
+    spawn_pad_ramp_3 = Entity(model='cube', rotation_x = 10, scale=(10,10,15), position=(0, -3.75, 11.5), texture='light_floor', texture_scale=Vec2(3,4), collider='mesh', color=color.gray, )
 
     global png
 
@@ -112,7 +113,8 @@ if __name__ == '__main__':
         double_sided=True,
         collision=True,
         position=Vec3(10, 10, 10),
-        collider='box'
+        collider='box',
+        
     )
 
     global player
@@ -120,7 +122,8 @@ if __name__ == '__main__':
         position=Vec3(0, 7, 0),
         speed=5,
         model='cube',
-        collider='mesh'
+        collider='mesh',
+        
     )
 
     showMenu()
